7d0b0dabc9763a5831b45e9745f7cf1234ddbc9a
r2hu1 <newasram86@gmail.com>
2025-07-13 00:15:54 +0530

    feat: dynamic skeleton loader for text

---
+++ components/text-skeleton.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "./ui/skeleton";
+
+export default function TextSkeleton({
+  text,
+  type = "p",
+  className,
+  skeletonClassName,
+}: {
+  text?: string | null;
+  type?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+  className?: string;
+  skeletonClassName?: string;
+}) {
+  const Slot = type;
+  return text ? (
+    <Slot className={`${className}`}>{text}</Slot>
+  ) : (
+    <Skeleton className={`h-2 w-20 ${skeletonClassName}`} />
+  );
+}
