973ac42e1a2955240182745c4f97e48e70226fd4
r2hu1 <newasram86@gmail.com>
2025-07-12 23:17:03 +0530

    feat: sidebar view (40%)

---
+++ modules/sidebar/views/ui/header.tsx
@@ -0,0 +1,31 @@
+import {
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Command } from "lucide-react";
+
+export default function DashboardSidebarHeader() {
+  return (
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton size="lg" asChild>
+            <div>
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <Command className="size-4" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">Cavio</span>
+                <span className="truncate text-xs">
+                  Write with steroids on!
+                </span>
+              </div>
+            </div>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  );
+}
---
+++ modules/sidebar/views/ui/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Command } from "lucide-react";
+import DashboardSidebarHeader from "./header";
+import { NavUser } from "./user";
+
+export function DashboardSidebar() {
+  return (
+    <Sidebar variant="inset">
+      <DashboardSidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
