ada8c183f884bd033615ea9f1fc30c64cb512e8c
r2hu1 <newasram86@gmail.com>
2025-07-12 23:16:02 +0530

    feat: added sidebar and gradient button

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
   return (
     <TRPCReactProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.className} antialiased`}>
           {children}
           <Toaster position="bottom-right" />
         </body>
