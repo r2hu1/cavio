24930ffec1ad41c68bca85de906968ab70ae3e43
r2hu1 <newasram86@gmail.com>
2025-07-14 21:26:58 +0530

    fix: rm redirect provider, updated middleware to filter instead

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,11 @@
-import { RedirectProvider } from "@/modules/auth/providers/redirect-provider";
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <RedirectProvider>
-      <div className="flex px-6 sm:px-0 h-full w-full absolute top-0 left-0 right-0 flex-col items-center justify-center">
-        {children}
-      </div>
-    </RedirectProvider>
+    <div className="flex px-6 sm:px-0 h-full w-full absolute top-0 left-0 right-0 flex-col items-center justify-center">
+      {children}
+    </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,21 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
 
-  if (!sessionCookie) {
+  const sessionDeProtectedRoutes = ["/auth/sign-in", "/auth/sign-up"];
+  console.log(request.nextUrl.pathname);
+  console.log(sessionDeProtectedRoutes.includes(request.nextUrl.pathname));
+  if (
+    sessionCookie &&
+    sessionDeProtectedRoutes.includes(request.nextUrl.pathname)
+  ) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/", "/pro", "/account", "/auth/sign-up", "/auth/sign-in"],
 };
