1eeef4e755c0c7b68864cd0ccf0574a82319a5c8
r2hu1 <newasram86@gmail.com>
2025-07-15 22:02:06 +0530

    feat: dummy search popup

--- modules/sidebar/views/ui/dashboard.tsx
+++ modules/sidebar/views/ui/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import {
   Sidebar,
   SidebarContent,
@@ -9,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import SearchFolderPopup from "@/modules/folders/views/ui/search-folder-popup";
 import { HomeIcon, Search } from "lucide-react";
 import Link from "next/link";
 
@@ -17,9 +19,17 @@ export default function DashboardNav() {
     <SidebarGroup className="mt-2">
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton tooltip={"Search"}>
-            <Search className="h-4 !w-4" /> Search
-          </SidebarMenuButton>
+          <SearchFolderPopup>
+            <SidebarMenuButton className="cursor-pointer" tooltip={"Search"}>
+              <Search className="h-4 !w-4" />{" "}
+              <span className="flex items-center justify-between w-full">
+                Search{" "}
+                <Badge className="text-[10px]" variant="outline">
+                  ⌘K
+                </Badge>
+              </span>
+            </SidebarMenuButton>
+          </SearchFolderPopup>
         </SidebarMenuItem>
         <SidebarMenuItem>
           <SidebarMenuButton asChild tooltip={"Home"}>
