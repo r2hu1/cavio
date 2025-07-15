8b5439378d6000819b04b824726e91f7f6fc2ef0
r2hu1 <newasram86@gmail.com>
2025-07-15 21:27:29 +0530

    feat: summy create folder popup

--- modules/folders/views/ui/create-folder-popup.tsx
+++ modules/folders/views/ui/create-folder-popup.tsx
@@ -13,34 +13,47 @@ import {
 } from "@/components/ui/credenza";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function CreateFolderPopup({
   children,
+  triggerClassName,
 }: {
   children: React.ReactNode;
+  triggerClassName?: string;
 }) {
+  // const trpc = useTRPC();
+  const handleCreateFolder = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const formData = new FormData(e.target as HTMLFormElement);
+    // const name = formData.get("folder-name") as string;
+    // await trpc.folder.create.mutate({ name });
+  };
   return (
     <Credenza>
-      <CredenzaTrigger asChild>{children}</CredenzaTrigger>
+      <CredenzaTrigger className={triggerClassName} asChild>
+        {children}
+      </CredenzaTrigger>
       <CredenzaContent>
         <CredenzaHeader>
           <CredenzaTitle>Create Folder</CredenzaTitle>
           <CredenzaDescription>
-            Quickly create a new folder and start writing your thoughts.
+            Quickly create a new folder in your workspace.
           </CredenzaDescription>
         </CredenzaHeader>
-        <CredenzaBody>
-          <form className="space-y-2">
+        <form className="space-y-4" onSubmit={handleCreateFolder}>
+          <CredenzaBody className="space-y-2">
             <Label htmlFor="folder-name">Folder Name</Label>
             <Input id="folder-name" type="text" placeholder="My Blogs" />
-          </form>
-        </CredenzaBody>
-        <CredenzaFooter>
-          <Button>Continue</Button>
-          <CredenzaClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </CredenzaClose>
-        </CredenzaFooter>
+          </CredenzaBody>
+          <CredenzaFooter>
+            <Button>Continue</Button>
+            <CredenzaClose asChild>
+              <Button variant="secondary">Cancel</Button>
+            </CredenzaClose>
+          </CredenzaFooter>
+        </form>
       </CredenzaContent>
     </Credenza>
   );
