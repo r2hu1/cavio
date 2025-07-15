9941611c7c7b6271cccd6d0dc49e7d65057dce60
r2hu1 <newasram86@gmail.com>
2025-07-15 23:40:54 +0530

    feat: home chat view

--- modules/home/views/ui/home-view.tsx
+++ modules/home/views/ui/home-view.tsx
@@ -1,7 +1,43 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import ChatInput from "@/modules/ai/views/ui/input";
+import { useAuthState } from "@/modules/auth/providers/auth-context";
+import { CornerDownLeft } from "lucide-react";
+
 export default function HomeView() {
+  const getGreetings = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    if (hours >= 0 && hours < 12) {
+      return "Good morning";
+    } else if (hours >= 12 && hours < 18) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  };
+  const { data: user, error, isPending } = useAuthState();
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center py-10 pt-20">
+      <div className="max-w-2xl">
+        <div className="max-w-lg mx-auto text-center space-y-4">
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-medium">
+            {getGreetings()}{" "}
+            <span className="font-bold text-indigo-700">
+              {!isPending && user?.user.name}
+            </span>
+            !
+          </h1>
+          <p className="text-sm sm:text-lg text-foreground/80">
+            Need content? Just ask. From blog posts to punchy copy, I’m your AI
+            writer ready to craft it all.
+          </p>
+        </div>
+        <div className="mt-10">
+          <ChatInput />
+        </div>
+      </div>
     </div>
   );
 }
