9a0e9019801b1ea82ad6cc138bf523ed6ee8303b
r2hu1 <newasram86@gmail.com>
2025-07-20 22:14:03 +0530

    feat: serilize nav

--- modules/header/views/ui/dynamic-nav.tsx
+++ modules/header/views/ui/dynamic-nav.tsx
@@ -1,16 +1,9 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import CreateDocumentPopup from "@/modules/documents/views/ui/create-document-popup";
-import FolderSettingsPopup from "@/modules/folders/views/ui/folder-settings-popup";
-import RenameFolderPopup from "@/modules/folders/views/ui/rename-folder-popup";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-import { Bolt, ClockFading, FilePlus, PencilLine } from "lucide-react";
+import FolderNav from "@/modules/folders/views/ui/folder-nav";
+import { Bolt, ClockFading, Share2 } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { toast } from "sonner";
 
 export default function DynamicNav() {
   const pathname = usePathname().split("/").filter(Boolean);
@@ -31,53 +24,16 @@ export default function DynamicNav() {
       )}
       {folderPage && <FolderNav folderId={folderId} />}
       {documentPage && (
-        <div className="flex items-center justify-end">
-          <Button size="icon" className="h-8 w-8" variant="secondary">
-            <ClockFading className="!h-3.5 !w-3.5" />
+        <div className="flex items-center justify-end gap-2">
+          <Button className="h-8 w-8" variant="secondary">
+            <Bolt className="!h-3.5 !w-3.5" />
+          </Button>
+          <Button className="h-8">
+            Share
+            <Share2 className="!h-3.5 !w-3.5" />
           </Button>
         </div>
       )}
     </div>
   );
 }
-
-const FolderNav = ({ folderId }: { folderId: string }) => {
-  const trpc = useTRPC();
-  const { data, isPending, error } = useQuery(
-    trpc.folder.getById.queryOptions({ id: folderId }),
-  );
-  useEffect(() => {
-    if (error) {
-      toast.error(error.message);
-    }
-  }, [isPending]);
-  return (
-    <div className="flex w-full items-center justify-between">
-      <div>
-        {!isPending ? (
-          <RenameFolderPopup folderId={folderId}>
-            <h1 className="text-sm flex items-center gap-2">
-              {data?.title}{" "}
-              <PencilLine className="cursor-pointer text-foreground/70 !h-3.5 !w-3.5" />
-            </h1>
-          </RenameFolderPopup>
-        ) : (
-          <Skeleton className="h-4 w-20" />
-        )}
-      </div>
-      <div className="items-center gap-2.5 flex">
-        <FolderSettingsPopup>
-          <Button className="h-8 w-8" variant="secondary">
-            <Bolt className="!h-3.5 !w-3.5" />
-          </Button>
-        </FolderSettingsPopup>
-        <CreateDocumentPopup folderId={folderId}>
-          <Button className="h-8">
-            New Document
-            <FilePlus className="!h-3.5 !w-3.5" />
-          </Button>
-        </CreateDocumentPopup>
-      </div>
-    </div>
-  );
-};
