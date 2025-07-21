ddb473fcd56e2303312804dd31f1fccaecdcaf72
r2hu1 <newasram86@gmail.com>
2025-07-21 22:48:36 +0530

    feat: procedures for edit, delet doc

--- modules/documents/server/procedures.ts
+++ modules/documents/server/procedures.ts
@@ -10,6 +10,7 @@ import { count, eq } from "drizzle-orm";
 import z from "zod";
 import { documentSchema } from "../schema";
 import { TRPCError } from "@trpc/server";
+import { title } from "process";
 
 export const documentsRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ input, ctx }) => {
@@ -41,5 +42,70 @@ export const documentsRouter = createTRPCRouter({
           message: "UNAUTHORIZED",
         });
       return document;
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const document = await db
+        .select()
+        .from(documents)
+        .where(eq(documents.id, input.id));
+      if (document.length > 0 && document[0].userId !== ctx.auth.user.id)
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "UNAUTHORIZED",
+        });
+      const deletedDocument = await db
+        .delete(documents)
+        .where(eq(documents.id, input.id))
+        .returning();
+      return deletedDocument;
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        title: z.string(),
+        content: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const document = await db
+        .select()
+        .from(documents)
+        .where(eq(documents.id, input.id));
+      if (document.length > 0 && document[0].userId !== ctx.auth.user.id)
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "UNAUTHORIZED",
+        });
+      const isContent = input.content !== undefined;
+      const [updatedDocument] = await db
+        .update(documents)
+        .set({
+          title: input.title,
+          content: isContent ? input.content : document[0].content,
+        })
+        .where(eq(documents.id, input.id))
+        .returning();
+      return [updatedDocument];
+    }),
+  get: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const document = await db
+        .select()
+        .from(documents)
+        .where(eq(documents.id, input.id));
+      if (document.length > 0 && document[0].userId !== ctx.auth.user.id)
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "UNAUTHORIZED",
+        });
+      return document[0];
     }),
 });
