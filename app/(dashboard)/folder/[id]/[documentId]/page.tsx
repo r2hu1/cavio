0ac948f244bb9eb3d8c2e5c8784db00528ca3490
r2hu1 <newasram86@gmail.com>
2025-07-31 13:48:27 +0530

    feat: dynamic metadata

--- app/(dashboard)/folder/[id]/[documentId]/page.tsx
+++ app/(dashboard)/folder/[id]/[documentId]/page.tsx
@@ -1,6 +1,22 @@
 import DocumentPageView from "@/modules/documents/views/ui/document-page-view";
 import PageLoader from "@/modules/preloader/views/ui/page-loader";
+import { createTRPCContext } from "@/trpc/init";
+import { appRouter } from "@/trpc/routers/_app";
 import { Suspense } from "react";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { documentId: string };
+}) => {
+  const { documentId } = await params;
+  const caller = appRouter.createCaller(await createTRPCContext());
+  const doc = await caller.document.get({ id: documentId });
+  return {
+    title: doc.title,
+    description: `Personal document, updated on ${doc.updatedAt ? new Date(doc.updatedAt).toLocaleString() : "never"}`,
+  };
+};
 
 export default function DocumentPage() {
   return (
--- app/(dashboard)/folder/[id]/page.tsx
+++ app/(dashboard)/folder/[id]/page.tsx
@@ -1,6 +1,23 @@
 import FolderPageView from "@/modules/folders/views/ui/folder-page-view";
 import PageLoader from "@/modules/preloader/views/ui/page-loader";
+import { createTRPCContext } from "@/trpc/init";
+import { appRouter } from "@/trpc/routers/_app";
+import { trpc } from "@/trpc/server";
 import { Suspense } from "react";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  const { id } = await params;
+  const caller = appRouter.createCaller(await createTRPCContext());
+  const folder = await caller.folder.getById({ id });
+  return {
+    title: folder.title,
+    description: `Your Folder has ${folder?.documents?.length || 0} documents, updated on ${folder.updatedAt ? new Date(folder.updatedAt).toLocaleString() : "never"}`,
+  };
+};
 
 export default function FolderPage() {
   return (
