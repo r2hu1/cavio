319c8269a19051d69f560ca7cb408eafabc30500
r2hu1 <newasram86@gmail.com>
2025-08-01 15:13:28 +0530

    feat: moduled res

---
+++ app/api/ai/command/route.ts
@@ -0,0 +1,24 @@
+import type { NextRequest } from "next/server";
+import { generateText, streamText } from "ai";
+import { NextResponse } from "next/server";
+import { googleai } from "@/lib/google-ai";
+
+export async function POST(req: NextRequest) {
+  const { messages, system } = await req.json();
+
+  try {
+    const result = await streamText({
+      model: googleai("models/gemini-2.0-flash") as any,
+      prompt: messages[0].content,
+      system,
+    });
+
+    return result.toTextStreamResponse();
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to process AI request" },
+      { status: 500 },
+    );
+  }
+}
