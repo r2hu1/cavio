40daa51fb9ec46ca38106385b81304eddae91ec7
r2hu1 <newasram86@gmail.com>
2025-08-05 20:46:40 +0530

    feat: added static input

---
+++ app/(dashboard)/chat/layout.tsx
@@ -0,0 +1,14 @@
+import StaticInput from "@/modules/ai/views/ui/static-input";
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative">
+      {children}
+      <StaticInput />
+    </div>
+  );
+}
