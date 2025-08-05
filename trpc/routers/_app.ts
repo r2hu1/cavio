bb62b7a3e291ff356fd3cdc019678f17e1572cde
r2hu1 <newasram86@gmail.com>
2025-08-05 22:28:20 +0530

    faet: ai router

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -3,10 +3,12 @@ import { baseProcedure, createTRPCRouter } from "../init";
 import { premiumRouter } from "@/modules/premium/server/procedures";
 import { documentsRouter } from "@/modules/documents/server/procedures";
 import { foldersRouter } from "@/modules/folders/server/procedures";
+import { aiRouter } from "@/modules/ai/server/procedures";
 export const appRouter = createTRPCRouter({
   premium: premiumRouter,
   document: documentsRouter,
   folder: foldersRouter,
+  ai: aiRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
