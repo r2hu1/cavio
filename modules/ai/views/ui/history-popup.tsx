e5c172f39875d5936a74c83400638eab8354a748
r2hu1 <newasram86@gmail.com>
2025-08-06 21:34:49 +0530

    feat: dn

--- modules/ai/views/ui/history-popup.tsx
+++ modules/ai/views/ui/history-popup.tsx
@@ -63,35 +63,40 @@ export default function HistoryPopup({
       <SheetContent>
         <SheetHeader>
           <SheetTitle>History</SheetTitle>
-          <SheetDescription className="mt-10 grid gap-3">
-            {!isPending &&
-              data &&
-              data.map((item, index) => (
-                <div className="h-12 border hover:bg-sidebar transition cursor-pointer hover:text-foreground rounded-lg flex items-center justify-between px-3 group">
-                  <Link key={index} href={`/chat/${item.id}`}>
-                    <h1 className="text-sm sm:text-base">{item.title}</h1>
-                  </Link>
-                  <Button
-                    size="sm"
-                    className="h-8 w-8 hidden group-hover:flex !-mr-1"
-                    variant="outline"
-                    disabled={isDeleting}
-                    onClick={() => handleDelete(item.id)}
+          <SheetDescription asChild className="mt-10 grid gap-3">
+            <div>
+              {!isPending &&
+                data &&
+                data.map((item, index) => (
+                  <div
+                    key={index}
+                    className="h-12 border hover:bg-sidebar transition cursor-pointer hover:text-foreground rounded-lg flex items-center justify-between px-3 group"
                   >
-                    <Trash className="!h-3.5 !w-3.5" />
-                  </Button>
+                    <Link href={`/chat/${item.id}`}>
+                      <h1 className="text-sm sm:text-base">{item.title}</h1>
+                    </Link>
+                    <Button
+                      size="sm"
+                      className="h-8 w-8 hidden group-hover:flex !-mr-1"
+                      variant="outline"
+                      disabled={isDeleting}
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      <Trash className="!h-3.5 !w-3.5" />
+                    </Button>
+                  </div>
+                ))}
+              {isPending && (
+                <div className="flex items-center h-32 justify-center">
+                  <Loader2 className="h-4 w-4 animate-spin" />
                 </div>
-              ))}
-            {isPending && (
-              <div className="flex items-center h-32 justify-center">
-                <Loader2 className="h-4 w-4 animate-spin" />
-              </div>
-            )}
-            {!isPending && !data?.length && (
-              <div className="flex border rounded-lg items-center h-32 justify-center">
-                <p className="text-sm text-foreground/80">Nothing yet :(</p>
-              </div>
-            )}
+              )}
+              {!isPending && !data?.length && (
+                <div className="flex border rounded-lg items-center h-32 justify-center">
+                  <p className="text-sm text-foreground/80">Nothing yet :(</p>
+                </div>
+              )}
+            </div>
           </SheetDescription>
         </SheetHeader>
         <SheetFooter>
