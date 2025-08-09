57249fb77ea7606fcbccbe862e4e7f370001fd6c
r2hu1 <newasram86@gmail.com>
2025-08-09 22:08:17 +0530

    feat: tweak instance connection

--- db/client.ts
+++ db/client.ts
@@ -5,6 +5,7 @@ import * as schema from "./schema";
 const { Pool } = pkg;
 
 declare global {
+  var _pgPool: InstanceType<typeof Pool> | undefined;
   var _drizzleDb: ReturnType<typeof drizzle> | undefined;
 }
 
@@ -12,11 +13,13 @@ const pool =
   global._pgPool ??
   new Pool({
     connectionString: process.env.DATABASE_URL,
+    idleTimeoutMillis: 30000,
   });
 
 if (!global._pgPool) global._pgPool = pool;
 
 const db = global._drizzleDb ?? drizzle(pool, { schema });
+
 if (!global._drizzleDb) global._drizzleDb = db;
 
 export { db };
