c0f6a0f54d206829d3396c5bead3bc23ee35a6fb
Rahul Rajput <newasram86@gmail.com>
2025-09-27 12:58:35 +0530

    feat: delete user acc and data

--- app/(dashboard)/settings/danger/page.tsx
+++ app/(dashboard)/settings/danger/page.tsx
@@ -1,5 +1,5 @@
 import PageLoader from "@/modules/preloader/views/ui/page-loader";
-import DangerPageView from "@/modules/settings/views/danger-page.view";
+import DangerPageView from "@/modules/settings/views/ui/danger-page.view";
 import { Suspense } from "react";
 
 export default async function SessionsPage() {
