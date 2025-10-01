51d8208f2ffc43a0f37be36ada80f959af930c7d
Rahul Rajput <newasram86@gmail.com>
2025-10-01 20:04:01 +0530

    fix: fking css

--- components/ui/editor.tsx
+++ components/ui/editor.tsx
@@ -79,7 +79,7 @@ const editorVariants = cva(
         default:
           "size-full px-16 pt-4 pb-72 text-base sm:px-[max(64px,calc(50%-350px))]",
         demo: "size-full px-16 pt-4 pb-72 text-base sm:px-[max(64px,calc(50%-350px))]",
-        fullWidth: "size-full px-16 pt-4 pb-72 text-base sm:px-24",
+        fullWidth: "size-full px-6 sm:pt-4 sm:pb-72 text-base sm:px-24",
         none: "",
         select: "px-3 py-2 text-base data-readonly:w-fit",
       },
--- modules/editor/views/ui/editor.tsx
+++ modules/editor/views/ui/editor.tsx
@@ -13,77 +13,77 @@ import { MarkdownPlugin } from "@platejs/markdown";
 import AiPopup from "@/modules/ai/views/ui/ai-popup";
 
 export default function Editor({ id }: { id: string }) {
-	const [defaultValue, setDefaultValue] = useState<any>([]);
+  const [defaultValue, setDefaultValue] = useState<any>([]);
 
-	const [value, setValue] = useState<any>(defaultValue);
-	const debouncedValue = useDebounce(value, 2000);
-	const trpc = useTRPC();
+  const [value, setValue] = useState<any>(defaultValue);
+  const debouncedValue = useDebounce(value, 2000);
+  const trpc = useTRPC();
 
-	const { data, isPending } = useQuery(trpc.document.get.queryOptions({ id }));
-	const { mutate } = useMutation(trpc.document.update.mutationOptions());
-	const { setState } = useEditorState();
+  const { data, isPending } = useQuery(trpc.document.get.queryOptions({ id }));
+  const { mutate } = useMutation(trpc.document.update.mutationOptions());
+  const { setState } = useEditorState();
 
-	const handleValueChange = useCallback((event: any) => {
-		const markdownValue = editor.api.markdown.serialize();
-		if (markdownValue) {
-			setValue(markdownValue);
-		}
-	}, []);
+  const handleValueChange = useCallback((event: any) => {
+    const markdownValue = editor.api.markdown.serialize();
+    if (markdownValue) {
+      setValue(markdownValue);
+    }
+  }, []);
 
-	const editor = usePlateEditor({
-		plugins: [...CopilotKit, ...EditorKit],
-		autoSelect: "end",
-		//@ts-ignore
-		id: "main",
-		//@ts-ignore
-		shouldInitialize: false,
-	});
+  const editor = usePlateEditor({
+    plugins: [...CopilotKit, ...EditorKit],
+    autoSelect: "end",
+    //@ts-ignore
+    id: "main",
+    //@ts-ignore
+    shouldInitialize: false,
+  });
 
-	useEffectOnce(() => {
-		if (!isPending && data?.content) {
-			setDefaultValue(data.content);
-			editor.tf.init({
-				value: (editor: any) =>
-					editor.getApi(MarkdownPlugin).markdown.deserialize(data.content),
-				autoSelect: "end",
-			});
-		}
-	}, [isPending, data]);
+  useEffectOnce(() => {
+    if (!isPending && data?.content) {
+      setDefaultValue(data.content);
+      editor.tf.init({
+        value: (editor: any) =>
+          editor.getApi(MarkdownPlugin).markdown.deserialize(data.content),
+        autoSelect: "end",
+      });
+    }
+  }, [isPending, data]);
 
-	useEffect(() => {
-		if (defaultValue != debouncedValue && defaultValue !== value) {
-			setState(true);
-			mutate(
-				{ id, content: debouncedValue },
-				{
-					onSettled: () => {
-						setState(false);
-					},
-				},
-			);
-		}
-	}, [debouncedValue]);
+  useEffect(() => {
+    if (defaultValue != debouncedValue && defaultValue !== value) {
+      setState(true);
+      mutate(
+        { id, content: debouncedValue },
+        {
+          onSettled: () => {
+            setState(false);
+          },
+        },
+      );
+    }
+  }, [debouncedValue]);
 
-	if (isPending) {
-		return <PageLoader />;
-	}
-	return (
-		<Suspense fallback={<PageLoader />}>
-			<Plate editor={editor} onValueChange={handleValueChange}>
-				<EditorContainer variant="default">
-					<EditorPlate
-						spellCheck={false}
-						defaultValue={defaultValue}
-						variant="ai"
-						placeholder="Click here to start writing."
-					/>
-				</EditorContainer>
-				<AiPopup
-					insert={""}
-					lastEdited={value.slice(-300)}
-					title={data?.title}
-				/>
-			</Plate>
-		</Suspense>
-	);
+  if (isPending) {
+    return <PageLoader />;
+  }
+  return (
+    <Suspense fallback={<PageLoader />}>
+      <Plate editor={editor} onValueChange={handleValueChange}>
+        <EditorContainer variant="default">
+          <EditorPlate
+            spellCheck={false}
+            defaultValue={defaultValue}
+            variant="fullWidth"
+            placeholder="Click here to start writing."
+          />
+        </EditorContainer>
+        <AiPopup
+          insert={""}
+          lastEdited={value.slice(-300)}
+          title={data?.title}
+        />
+      </Plate>
+    </Suspense>
+  );
 }
