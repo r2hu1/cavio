eb473ba36cb15b13d6db5fc97bf65179e0bf6271
r2hu1 <newasram86@gmail.com>
2026-02-11 23:40:51 +0530

    rm aurora

--- modules/folders/views/ui/folder-page-view.tsx
+++ modules/folders/views/ui/folder-page-view.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { AuroraText } from "@/components/ui/aurora-text";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
