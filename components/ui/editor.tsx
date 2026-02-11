121795ccff46db76e8c8bdbd68bfc1e26667226f
r2hu1 <newasram86@gmail.com>
2026-02-11 23:42:54 +0530

    feat: more padding

--- components/ui/editor.tsx
+++ components/ui/editor.tsx
@@ -72,7 +72,7 @@ const editorVariants = cva(
         true: "ring-2 ring-ring ring-offset-2",
       },
       variant: {
-        ai: "w-full sm:px-10 py-10 text-base",
+        ai: "w-full sm:px-12 py-14 text-base",
         aiChat:
           "max-h-[min(70vh,320px)] w-full overflow-y-auto px-3 py-2 text-base md:text-sm",
         comment: cn("rounded-none border-none bg-transparent text-sm"),
