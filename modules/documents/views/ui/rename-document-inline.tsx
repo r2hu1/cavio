710c20262933751f97dac624295f077c16bd8a23
r2hu1 <newasram86@gmail.com>
2026-02-13 23:10:12 +0530

    feat: fix uc

--- modules/documents/views/ui/rename-document-inline.tsx
+++ modules/documents/views/ui/rename-document-inline.tsx
@@ -11,104 +11,104 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function RenameDocumentInline({
-	documentId,
-	folderId,
-	textClassName,
-	inputClassName,
+  documentId,
+  folderId,
+  textClassName,
+  inputClassName,
 }: {
-	documentId: string;
-	folderId: string;
-	textClassName?: string;
-	inputClassName?: string;
+  documentId: string;
+  folderId: string;
+  textClassName?: string;
+  inputClassName?: string;
 }) {
-	const trpc = useTRPC();
-	const queryClient = useQueryClient();
-	const { mutate } = useMutation(trpc.document.update.mutationOptions());
-	const { data, isPending, error } = useQuery(
-		trpc.document.get.queryOptions({ id: documentId }),
-	);
-	const router = useRouter();
-	useEffect(() => {
-		if (error) {
-			if (error.message.split(" ").includes("undefined")) {
-				router.push("/");
-			}
-			if (error.message == "UNAUTHORIZED") {
-				toast.error("You are not authorized to view this document.");
-				router.push("/");
-			}
-		}
-		if (data) {
-			setNewDocumentName(data.title);
-		}
-	}, [isPending]);
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(trpc.document.update.mutationOptions());
+  const { data, isPending, error } = useQuery(
+    trpc.document.get.queryOptions({ id: documentId }),
+  );
+  const router = useRouter();
+  useEffect(() => {
+    if (error) {
+      if (error.message.split(" ").includes("undefined")) {
+        router.push("/");
+      }
+      if (error.message == "UNAUTHORIZED") {
+        toast.error("You are not authorized to view this document.");
+        router.push("/");
+      }
+    }
+    if (data) {
+      setNewDocumentName(data.title);
+    }
+  }, [isPending]);
 
-	const [isEditing, setIsEditing] = useState(false);
-	const [newDocumentName, setNewDocumentName] = useState("");
-	const [loading, setLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newDocumentName, setNewDocumentName] = useState("");
+  const [loading, setLoading] = useState(false);
 
-	const handleSave = async () => {
-		if (newDocumentName.length < 5 || newDocumentName == data?.title) {
-			setIsEditing(false);
-			return;
-		}
-		setLoading(true);
-		setIsEditing(false);
-		mutate(
-			{
-				title: newDocumentName,
-				id: documentId,
-			},
-			{
-				onSuccess: async () => {
-					await queryClient.invalidateQueries(
-						trpc.document.getAllByFolderId.queryOptions({ folderId }),
-					);
-					await queryClient.invalidateQueries(
-						trpc.document.get.queryOptions({ id: documentId }),
-					);
-					await queryClient.invalidateQueries(
-						trpc.document.getRecent.queryOptions(),
-					);
-				},
-				onError: (error) => {
-					toast.error(error.message);
-				},
-				onSettled: () => {
-					setLoading(false);
-					setIsEditing(false);
-				},
-			},
-		);
-	};
+  const handleSave = async () => {
+    if (newDocumentName.length < 5 || newDocumentName == data?.title) {
+      setIsEditing(false);
+      return;
+    }
+    setLoading(true);
+    setIsEditing(false);
+    mutate(
+      {
+        title: newDocumentName,
+        id: documentId,
+      },
+      {
+        onSuccess: async () => {
+          await queryClient.invalidateQueries(
+            trpc.document.getAllByFolderId.queryOptions({ folderId }),
+          );
+          await queryClient.invalidateQueries(
+            trpc.document.get.queryOptions({ id: documentId }),
+          );
+          await queryClient.invalidateQueries(
+            trpc.document.getRecent.queryOptions(),
+          );
+        },
+        onError: (error) => {
+          toast.error(error.message);
+        },
+        onSettled: () => {
+          setLoading(false);
+          setIsEditing(false);
+        },
+      },
+    );
+  };
 
-	return isPending ? (
-		<Skeleton className="h-4 w-20" />
-	) : isEditing ? (
-		<Input
-			className={cn("w-[150px]", inputClassName)}
-			autoFocus
-			value={newDocumentName}
-			onChange={(e) => setNewDocumentName(e.target.value)}
-			onBlur={handleSave}
-			onKeyDown={(e) => e.key == "Enter" && handleSave()}
-			onKeyUp={(e) => e.key == "Escape" && setIsEditing(false)}
-		/>
-	) : (
-		<div
-			onClick={() => setIsEditing(!isEditing)}
-			className={cn(
-				"flex items-center gap-2",
-				loading && "animate-pulse",
-				textClassName,
-			)}
-		>
-			<h1 className="flex items-center gap-2 !w-[100px] sm:!w-full truncate">
-				{data?.title}
-			</h1>
-			<Tooltip text="Rename Document">
-				<PencilLine className="!h-3.5 cursor-pointer !w-3.5 text-foreground/70" />
-			</Tooltip>
-		</div>
-	);
+  return isPending ? (
+    <Skeleton className="h-4 w-20" />
+  ) : isEditing ? (
+    <Input
+      className={cn("w-[150px]", inputClassName)}
+      autoFocus
+      value={newDocumentName}
+      onChange={(e) => setNewDocumentName(e.target.value)}
+      onBlur={handleSave}
+      onKeyDown={(e) => e.key == "Enter" && handleSave()}
+      onKeyUp={(e) => e.key == "Escape" && setIsEditing(false)}
+    />
+  ) : (
+    <div
+      onClick={() => setIsEditing(!isEditing)}
+      className={cn(
+        "flex items-center gap-2",
+        loading && "animate-pulse",
+        textClassName,
+      )}
+    >
+      <h1 className="flex items-center gap-2 !w-fit sm:!w-full truncate">
+        {data?.title}
+      </h1>
+      <Tooltip text="Rename Document">
+        <PencilLine className="!h-3.5 cursor-pointer !w-3.5 text-foreground/70" />
+      </Tooltip>
+    </div>
+  );
 }
