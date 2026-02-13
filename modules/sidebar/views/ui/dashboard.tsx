befefd38be3be0da47bd812700f3e0ee55bcecf2
r2hu1 <newasram86@gmail.com>
2026-02-13 12:13:22 +0530

    feat: polish

--- modules/sidebar/views/ui/dashboard.tsx
+++ modules/sidebar/views/ui/dashboard.tsx
@@ -10,6 +10,7 @@ import { HomeIcon, Search } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import SearchPopup from "./search-popup";
+import { Kbd } from "@/components/ui/kbd";
 
 export default function DashboardNav() {
   const pathname = usePathname();
@@ -22,13 +23,10 @@ export default function DashboardNav() {
             className="cursor-pointer"
             tooltip={"Search"}
           >
-            <SearchPopup triggerClassName="cursor-pointer flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none">
+            <SearchPopup triggerClassName="h-8 cursor-pointer flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none">
               <Search className="h-4 !w-4" />{" "}
               <span className="flex items-center justify-between w-full text-sidebar-accent-foreground/80 hover:text-sidebar-accent-foreground">
-                Search{" "}
-                <Badge className="text-[10px]" variant="outline">
-                  ⌘K
-                </Badge>
+                Search <Kbd>⌘K</Kbd>
               </span>
             </SearchPopup>
           </SidebarMenuButton>
